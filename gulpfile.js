d23a6e8ff6e4bedcf831df5f4a7635d9ea2895ce
Jim OKelly <jim@devschool.rocks>
2016-05-17 14:13:46 -0700

    ready to create collectPosts

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,78 @@
 var gulp           = require('gulp');
+//var plugins      = require('gulp-load-plugins');
 var data           = require('gulp-data');
+var sequence       = require('gulp-sequence');
+var sass           = require('gulp-sass');
+var concat         = require('gulp-concat');
+var uglify         = require('gulp-uglify');
 var nunjucksRender = require('gulp-nunjucks-render');
+var htmlmin        = require('gulp-htmlmin');
+var imagemin       = require('gulp-imagemin');
+var markdown       = require('gulp-markdown');
+var frontMatter    = require('gulp-front-matter');
+var bSync          = require('browser-sync');
+var reload         = bSync.reload;
 
 gulp.task('pages', function() {
-  return gulp.src("src/pages/**/*.html")
+  return gulp.src('src/pages/**/*.html')
              .pipe(nunjucksRender({
-               path: ["src/templates"]
+               path: ['src/templates']
              }))
-             .pipe(gulp.dest("dist"));
+             .pipe(htmlmin({collapseWhitespace: true}))
+             .pipe(gulp.dest('dist'))
+             .pipe(reload({stream: true}));
 });
 
-gulp.task('default', ['pages']);
+gulp.task('images', function() {
+  return gulp.src('src/img/*')
+             .pipe(imagemin())
+             .pipe(gulp.dest('dist/img'))
+             .pipe(reload({stream: true}));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*')
+             .pipe(uglify())
+             .pipe(gulp.dest('dist/js'))
+             .pipe(reload({stream: true}));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('src/sass/**/*.scss')
+             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+             .pipe(concat('all.min.css'))
+             .pipe(gulp.dest('dist/css'))
+             .pipe(reload({stream: true}));
+});
+
+gulp.task('posts', function() {
+  return gulp.src('src/posts/**/*.md')
+             .pipe(frontMatter({property: 'page', remove: true}))
+             .pipe(markdown())
+             .pipe(gulp.dest('dist/blog'))
+             .pipe(reload({stream: true}));
+});
+
+gulp.task('sync', function() {
+  return bSync({server: { baseDir: 'dist' }});
+});
+
+gulp.task('serve', function(cb) {
+   return sequence('sync', cb);
+});
+
+gulp.task('assets', ['images', 'styles', 'scripts']);
+
+gulp.task('content', function(cb) {
+  return sequence('posts', 'pages', cb);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/js/**/*.js',      [ 'scripts']);
+  gulp.watch('src/sass/**/*.scss',  [ 'styles']);
+  gulp.watch('src/img/**/*',        [ 'images']);
+  gulp.watch('src/posts/**/*.md',   [ 'content']);
+  gulp.watch('src/pages/**/*.html', [ 'pages']);
+});
+
+gulp.task('default', sequence(['assets', 'content'], 'serve', 'watch'));
---
+++ src/posts/my-first-blog-post.md
@@ -0,0 +1,12 @@
+---
+author: Erika
+title: My first blog post
+date: 2016/04/20
+---
+
+Lorem quam sit animi vitae commodi. Ut quidem vitae accusamus recusandae modi nulla libero. Est reprehenderit necessitatibus iste omnis sit laborum. Vel inventore quae corrupti saepe ab neque iste. Animi!
+
+
+## And then for something completely different!
+
+Amet officiis mollitia nulla officia expedita, voluptatibus earum eaque possimus! Omnis neque nobis voluptatibus at amet, quaerat sint cum quo sit nisi! Ut modi totam debitis illo voluptatem, nam non.
